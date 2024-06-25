3a0b898be4151e5eca0847f35eeb0479b6cfdfa4
Douglas Tescaro <52058114+douglas9932@users.noreply.github.com>
2024-06-25 08:38:17 -0300

    Update Navbar.tsx

--- src/Componentes/Navbar.tsx
+++ src/Componentes/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import Link from "react-router-dom";
+import {Link} from "react-router-dom";
 import imageLogo from '../Imagens/LogoStoll.png';
 
 export const Navbar = () => {
