5f76daf4418c4a0520b71cc61eb97b82ffd6b39d
Tescaro <tescarodouglas9@gmail.com>
2024-08-04 01:49:11 -0300

    Att para subir para o Servidor

--- client/src/Classes/API.tsx
+++ client/src/Classes/API.tsx
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 const api = Axios.create({
   // baseURL: 'http://localhost:32322', // Altere para a URL do seu servidor
-  baseURL: 'http://172.16.0.128:32322', // Altere para a URL do seu servidor
+  baseURL: 'http://92.112.176.30:32322', // Altere para a URL do seu servidor
 });
 
 const API = {
