e8e57ba107cca58105f41e1567d2d9454f0eba03
Douglas Tescaro <52058114+douglas9932@users.noreply.github.com>
2024-08-12 14:39:24 -0300

    Update API.tsx

--- src/Classes/API.tsx
+++ src/Classes/API.tsx
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-  baseURL: 'http://localhost:32322', // Altere para a URL do seu servidor
-  //baseURL: 'http://92.112.176.30:32322', // Altere para a URL do seu servidor
+  //baseURL: 'http://localhost:32322', // Altere para a URL do seu servidor
+  baseURL: 'http://92.112.176.30:32322', // Altere para a URL do seu servidor
 });
 
 const API = {
